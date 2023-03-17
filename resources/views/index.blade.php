ab7f223a17811967c0b939bf042c32dededc4391
Admin <roman.nikolaevich@gmail.com>
2023-03-17 12:53:57 +0200

    pagination

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
 
     <a class="btn btn-primary" role="button" href="{{ route('users.create') }} ">Create user</a>
 
-    <table class="table">
+    <table class="table table-sm">
         <thead>
         <tr>
             <th scope="col">#</th>
@@ -37,8 +37,8 @@
                 </td>
             </tr>
         @endforeach
-
-
         </tbody>
     </table>
+
+    {{ $users->links() }}
 @endsection
